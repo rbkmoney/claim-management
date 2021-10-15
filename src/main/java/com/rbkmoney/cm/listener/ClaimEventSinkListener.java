1b8ed62aa48eca54f6c9ffa555a818c62bb3e5f4
vitaxa <vitaxa93gamebox@gmail.com>
2021-10-15 11:25:22 +0300

    refactor claim event handler

--- src/main/java/com/rbkmoney/cm/listener/ClaimEventSinkListener.java
+++ src/main/java/com/rbkmoney/cm/listener/ClaimEventSinkListener.java
@@ -1,7 +1,6 @@
 package com.rbkmoney.cm.listener;
 
-import com.rbkmoney.cm.service.ClaimCommitterService;
-import com.rbkmoney.damsel.claim_management.ClaimStatusChanged;
+import com.rbkmoney.cm.handler.event.ClaimEventSinkManager;
 import com.rbkmoney.damsel.claim_management.Event;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,31 +13,18 @@ import org.springframework.messaging.handler.annotation.Header;
 @RequiredArgsConstructor
 public class ClaimEventSinkListener {
 
-    private final ClaimCommitterService claimCommitterService;
+    private final ClaimEventSinkManager claimEventSinkManager;
 
     @KafkaListener(topics = "${kafka.topics.claim-event-sink.id}", containerFactory = "kafkaListenerContainerFactory")
-    public void handle(Event event,
-                       @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
-                       @Header(KafkaHeaders.OFFSET) int offsets,
-                       Acknowledgment ack) {
-        if (event.getChange().isSetStatusChanged()) {
-            ClaimStatusChanged claimStatusChanged = event.getChange().getStatusChanged();
-            if (claimStatusChanged.getStatus().isSetPendingAcceptance()) {
-                log.info("Found event in 'pending_acceptance' status, event='{}'", event);
-                try {
-                    claimCommitterService.doCommitClaim(
-                            claimStatusChanged.getPartyId(),
-                            claimStatusChanged.getId(),
-                            claimStatusChanged.getRevision()
-                    );
-                    ack.acknowledge();
-                    log.info("Event have been processed, event='{}'", event);
-                } catch (Exception ex) {
-                    log.error("Failed to process event, event='{}'", event, ex);
-                    throw ex;
-                }
-            }
-        }
+    public void handle(
+            Event event,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+            @Header(KafkaHeaders.OFFSET) int offsets,
+            Acknowledgment ack
+    ) {
+        log.info("Handle event={}; partition={}; offsets={}", event, partition, offsets);
+        claimEventSinkManager.handleEvent(event, ack);
+        log.info("Event have been processed, event={}; partition={}; offsets={}", event, partition, offsets);
     }
 
 }
